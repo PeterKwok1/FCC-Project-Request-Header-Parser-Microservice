d93ec82735e1b20dde95d8f55af7a5ecf4dd5a2d
Peter Kwok <peter.kwok94@gmail.com>
2024-02-25 17:06:14 -0500

    feat: complete

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// my code
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+})
